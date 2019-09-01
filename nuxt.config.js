460472ec62dca00b31d957f3b58250d480a5b31f
小卡 <GitMeimz.com>
2019-09-01 15:51:25 +0800

    第一次提交

---
+++ nuxt.config.js
@@ -0,0 +1,67 @@
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: "旅游攻略" || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', type: 'text/less', href: '//at.alicdn.com/t/font_1168872_ehvuah8v57g.css' } // 新增全局字体样式
+    ],
+    css: [
+      "assets/main.css"//新增自定义的页面过度样式
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    'element-ui/lib/theme-chalk/index.css'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '@/plugins/element-ui'
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    transpile: [/^element-ui/],
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  }
+}
---
+++ pages/tabs.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="tabs" style="margin-left:100px">
+    <span v-for="(item,index) in ['新闻','体育','娱乐']" :key="index" @click="tabs(index)" :class="{active:curren === index}">{{item}}</span>
+
+    <div>
+      <ul v-show="curren === 0">
+        <li>中秋将至</li>
+        <li>中秋将至</li>
+        <li>中秋将至</li>
+      </ul>
+    </div>
+    <div>
+      <ul v-show="curren === 1">
+        <li>中秋将至11</li>
+        <li>中秋将至11</li>
+        <li>中秋将至11</li>
+      </ul>
+    </div>
+    <div>
+      <ul  v-show="curren === 2">
+        <li>中秋将至22</li>
+        <li>中秋将至22</li>
+        <li>中秋将至22</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+       return{
+           curren:0
+       }
+    },
+    methods:{
+         tabs(index){
+             this.curren=index
+         }
+    }
+};
+</script>
+
+<style lang=less scoped>
+   span{
+       cursor: pointer;
+       padding: 10px;
+   }
+   .active{
+       color: red;
+   }
+</style>
